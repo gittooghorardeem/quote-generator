bd034df5ed45da1568b5beb0d3970f07fd9bd300
gittooghorardeem <ghorar.dim.gittoo@gmail.com>
2021-10-19 19:43:23 +0600

    Dev - Loader animaton and Tutorial COMPLETE

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 // Show new Quote
 function newQuote() {
+    loading();
     // Pick a random quote from API apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // const test = Math.random();
@@ -29,11 +43,15 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote'); // Add CSS styling class
     }
+
+    // Set the quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+    loading();
     const apiurl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiurl);
